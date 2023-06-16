b56b90dabde1d943917721a9d3dd18cdd5583441
raxidbou4nja <official.wetyn@gmail.com>
2023-06-16 14:42:31 -0700

    add new file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Printing lowercase and uppercase alphabet..
+ * Return: 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
