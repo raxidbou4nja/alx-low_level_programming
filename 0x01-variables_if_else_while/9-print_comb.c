c5e0232bc0c2268d475d4d40a2007551f02fd082
raxidbou4nja <official.wetyn@gmail.com>
2023-06-16 16:30:58 -0700

    add new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 9 with colon and space
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int  number;
+
+	for (number = 0; number < 10; number++)
+	}
+		putchar(number + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
