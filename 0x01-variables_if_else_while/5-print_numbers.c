6ab9325f3127dbd9f2a63a74251a6c89e30bdd5b
raxidbou4nja <official.wetyn@gmail.com>
2023-06-16 15:48:29 -0700

    add file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-	int number = 0;
-	for (number = 0; number <= 9; number++)
-	{
-		printf("%d", number);
-	}
-	putchar('\n');
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", num);
+
+	printf("\n");
+
 	return (0);
 }
