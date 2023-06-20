4b1e2f897c34b3bf6b305ed29b65ace8ddf02a88
raxidbou4nja <official.wetyn@gmail.com>
2023-06-20 14:34:44 -0700

    add new file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,14 @@
  * Return: 1 or 0
  */
 int _putchar(char c);
+/**
+ * print_alphabet - function to print alphabet
+ *
+ */
 void print_alphabet(void);
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void);
 #endif
