63ddddbab00508bd847880643805a34188cda18c
raxidbou4nja <official.wetyn@gmail.com>
2023-06-20 16:04:45 -0700

    add new file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -35,4 +35,10 @@ int _isalpha(int c);
  * Return: 1 or 0 or -1
 */
 int print_sign(int n);
+/**
+ * _abs - print absolute value
+ * @int: integer
+ * Return: integer or abs value of int
+ */
+int _abs(int);
 #endif
