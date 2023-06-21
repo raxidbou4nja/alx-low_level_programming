d6f3de409a57cfde7fb6c6a8d3ed9afa0c87d0d0
raxidbou4nja <official.wetyn@gmail.com>
2023-06-20 17:42:09 -0700

    add new file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - prints times table
+ *
+*/
+void times_table(void)
+{
+	int n;
+	int m;
+	int r;
+
+	for (n = 0; n <= 9; n++)
+	{
+			for (m = 0; m <= 9; m++)
+			{
+
+					r = n * m;
+
+				if (r < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(r / 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(r / 10 + '0');
+					_putchar(r % 10 + '0');
+				}
+
+				if(m < 9)
+				{
+					_putchar(',');
+				}
+
+			}
+		_putchar('\n');
+	}
+}
