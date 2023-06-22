8c4c67a6a614e78d31eabb6e9a0a5b08d7a6d1e1
raxidbou4nja <official.wetyn@gmail.com>
2023-06-22 12:24:10 -0700

    add new file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - return is zero when input is between a-z
+ * @c: parameter
+ * Return: Always 0
+*/
+
+int _isupper(int c)
+{
+
+	if ( c <= 'Z'  && c >= 'A')
+	{
+		return 1;
+	}
+		return 0;
+}
