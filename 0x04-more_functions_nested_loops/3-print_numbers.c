afd54a7a0a9f0495e9983243a55b41b95af483e1
raxidbou4nja <official.wetyn@gmail.com>
2023-06-22 12:56:42 -0700

    add new file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: Always 0
+*/
+
+void print_numbers(void)
+{
+
+	int number = 0;
+
+	while (number < 10){
+		_putchar((number % 10) + '0');
+		number++;
+	}
+
+	_putchar('\n');
+
+}
