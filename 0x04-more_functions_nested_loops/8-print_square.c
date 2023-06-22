84460682890a20e951664c986fc5e18d64a94a3a
raxidbou4nja <official.wetyn@gmail.com>
2023-06-22 14:29:31 -0700

    add new file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * @size: param.
+ */
+void print_square(int size)
+{
+	int height;
+	int width;
+
+
+	if (size > 0)
+	{
+		for (height = 0; height < size; height++)
+		{
+			for (width = 0; width < size; width++)
+			{
+				_putchar('#');
+			}
+
+			if (height == size - 1)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
