920681eb7029177c7dc0de4aaafe4e613b9756b4
raxidbou4nja <official.wetyn@gmail.com>
2023-06-22 13:37:17 -0700

    add file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,22 @@
 
 void more_numbers(void)
 {
-
 	int number = 0;
+	int i;
 
 	while (number <= 10)
 	{
-		for (int i = 0; i <= 14; ++i)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar((number % 10) + '0');
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
 		number++;
-	}
 
-	_putchar('\n');
+		_putchar('\n');
+
+	}
 }
