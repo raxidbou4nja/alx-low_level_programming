1283f6d018ce7b968bb90d9cace569345f84e016
raxidbou4nja <official.wetyn@gmail.com>
2023-06-26 12:20:09 -0700

    update betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 /**
  * reset_to_98 -  takes a pointer to an int as parameter
  *          and updates the value it points to to 98
- *@n= param
+ * @n: pointer parameter
  */
 void reset_to_98(int *n)
 {
