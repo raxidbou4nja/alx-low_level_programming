510dc1993e13b737f1dad711fbf86137438a421a
raxidbou4nja <official.wetyn@gmail.com>
2023-06-26 12:16:17 -0700

    fix betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main -   takes a pointer to an int as parameter 
+ * reset_to_98 -  takes a pointer to an int as parameter
  *          and updates the value it points to to 98
  *@n= param
  */
