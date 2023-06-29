957a9309ac1a2e3d8d33b52132d686ebc194e172
raxidbou4nja <official.wetyn@gmail.com>
2023-06-29 16:34:46 -0700

    add new file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,21 +12,19 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0;
-	int src_len = 0;
+	int index = 0, srcLen = 0;
 
-	while (src[index] != '\0')
+	while (src[index++])
 	{
-		src_len++;
-		index++;
+		srcLen++;
 	}
 
-	for (index = 0; src[index] != '\0' && index < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 	{
 		dest[index] = src[index];
 	}
 
-	for (; index < n; index++)
+	for (index = srcLen; index < n; index++)
 	{
 		dest[index] = '\0';
 	}
