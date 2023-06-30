2dc7c97fe9ec3b6679130e9e20128de078258fef
raxidbou4nja <official.wetyn@gmail.com>
2023-06-29 18:15:36 -0700

    add new file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: param of string
+ *
+ * Return: string.
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] <= 'z' && str[index] >= 'a'))
+		{
+			index++;
+		}
+
+			if (str[index - 1] == ' ' ||
+			str[index - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+			{
+				str[index] -= 32;
+			}
+
+		index++;
+	}
+
+	return (str);
+}
