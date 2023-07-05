f404da650d7462f5d7c05f2d2785d78eafa6150a
raxidbou4nja <official.wetyn@gmail.com>
2023-07-05 15:14:37 -0700

    add new file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,19 +2,21 @@
 /**
  * factorial - the factorial of a given number
  * @n: pointer of given number.
- * return: facturial
+ * Return: facturial
 */
 
 int factorial(int n)
 {
-	if (n < 0) {
-		return -1;
+	if (n < 0)
+	{
+		return (-1);
 	}
 
-	if (n == 0) {
-		return 1;
+	if (n == 0)
+	{
+		return (1);
 	}
 
-	return n * factorial(n - 1);
+	return (n * factorial(n - 1));
 
 }
