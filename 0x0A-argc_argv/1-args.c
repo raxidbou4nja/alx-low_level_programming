f454eca1cb0f7b0dbc432ba2ba0f9c28236c2e46
raxidbou4nja <official.wetyn@gmail.com>
2023-07-07 14:57:17 -0700

    add new file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints length of params.
+ * @argc: lenght of params.
+ * @argv: array of params.
+ *
+ * Return: length of params
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
