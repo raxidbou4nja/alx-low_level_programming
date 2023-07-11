60a11304c59f7b04489b9d2276ff14fa905d0227
raxidbou4nja <official.wetyn@gmail.com>
2023-07-11 12:50:18 -0700

    add new file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,26 +20,33 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	grid = (int **)malloc(height * sizeof(int *));
+	grid = (int **)malloc(sizeof(int *) * height);
 
 	if (grid == NULL)
 	{
-		 return (NULL);
+		return (NULL);
 	}
-		for (i = 0; i < height; i++)
-		{
-			grid[i] = (int *)calloc(width, sizeof(int));
 
-			if (grid[i] == NULL)
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
 			{
-				for (j = 0; j < i; j++)
-				{
-					free(grid[j]);
-				}
-				free(grid);
-				return (NULL);
+				free(grid[j]);
 			}
+			free(grid);
 		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
 
 	return (grid);
 }
