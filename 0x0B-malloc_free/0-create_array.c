76b4a66ca119d6e92c2a54a5b5f79316b351ad31
raxidbou4nja <official.wetyn@gmail.com>
2023-07-11 08:46:21 -0700

    add new file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,9 @@
 char *create_array(unsigned int size, char c)
 {
 
-	char *array = malloc((size + 1) * sizeof(char));
+	char *array = malloc((size) * sizeof(char));
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
 		return (NULL);
