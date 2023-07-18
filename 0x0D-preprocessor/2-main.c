b17c5c01dbb0746d1d7a98b3f104f21817593bba
raxidbou4nja <official.wetyn@gmail.com>
2023-07-17 17:11:55 -0700

    add new file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main file returns file name
+ * Return: returns 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
