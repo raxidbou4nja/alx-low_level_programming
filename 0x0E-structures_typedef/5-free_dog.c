96c16d70dda153bed6df567feb2e37c3f04fee4b
raxidbou4nja <official.wetyn@gmail.com>
2023-07-17 18:31:48 -0700

    add new file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - to Freeing dogs.
+ * @d: The dog to be freed.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
