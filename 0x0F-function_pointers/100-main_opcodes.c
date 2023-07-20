2bae94b33bbb54935cca7a515321c0dd049779a9
raxidbou4nja <official.wetyn@gmail.com>
2023-07-19 18:53:18 -0700

    add new file

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,29 +11,34 @@
  */
 int main(int argc, char *argv[])
 {
-	int num_bytes;
-	char *main_ptr;
-	int i;
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
-	num_bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-	if (num_bytes < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
-	main_ptr = (char *)&main;
-
-	for (i = 0; i < num_bytes; i++)
+	for (index = 0; index < bytes; index++)
 	{
-		printf("%.2hhx ", main_ptr[i]);
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
 	}
 
 	printf("\n");
