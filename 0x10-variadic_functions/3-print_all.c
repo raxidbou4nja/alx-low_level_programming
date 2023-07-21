3bc68ec1061b893181c30bbde2b63e4f2f0b81da
raxidbou4nja <official.wetyn@gmail.com>
2023-07-20 18:55:34 -0700

    add new file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_int - prints an int
- * @args: list of arguments
+ * @arguments: list of arguments
  */
-void print_int(va_list args)
+void print_int(va_list arguments)
 {
-	printf("%d", va_arg(args, int));
+	printf("%d", va_arg(arguments, int));
 }
 /**
  * print_char - prints a char
- * @args: list of arguments
+ * @arguments: list of arguments
  */
-void print_char(va_list args)
+void print_char(va_list arguments)
 {
-	printf("%c", va_arg(args, int));
+	printf("%c", va_arg(arguments, int));
 }
 /**
  * print_string - prints a string
- * @args: list of arguments
+ * @arguments: list of arguments
  */
-void print_string(va_list args)
+void print_string(va_list arguments)
 {
-	char *str = va_arg(args, char *);
+	char *z = va_arg(arguments, char *);
 
-	if (str == NULL)
+	if (!z)
 	{
 		printf("(nil)");
+		return;
 	}
-	else
-	{
-		printf("%s", str);
-	}
+	printf("%s", z);
 }
 /**
  * print_float - prints floats
- * @args: list of arguments
+ * @arguments: list of arguments
  */
-void print_float(va_list args)
+void print_float(va_list arguments)
 {
-	double value = va_arg(args, double);
-
-	printf("%f", value);
+	printf("%f", va_arg(arguments, double));
 }
 /**
  * print_all - prints all
@@ -59,26 +55,26 @@ void print_all(const char * const format, ...)
 	{'s', print_string},
 	{'\0', NULL}
 	};
-	va_list args;
+	va_list arguments;
 	char *separator1 = "", *separator2 = ", ";
-	int count1 = 0, count2 = 0;
+	int counter1 = 0, counter2 = 0;
 
-	va_start(args, format);
-	while (format !=  NULL && format[count1] != '\0')
+	va_start(arguments, format);
+	while (format !=  NULL && format[counter1] != '\0')
 	{
-		count2 = 0;
-		while (types[count2].type != '\0')
+		counter2 = 0;
+		while (types[counter2].type != '\0')
 		{
-			if (format[count1] == types[count2].type)
+			if (format[counter1] == types[counter2].type)
 			{
 				printf("%s", separator1);
-				types[count2].printer(args);
+				types[counter2].printer(arguments);
 				separator1 = separator2;
 			}
-			count2++;
+			counter2++;
 		}
-		count1++;
+		counter1++;
 	}
 	printf("\n");
-	va_end(args);
+	va_end(arguments);
 }
