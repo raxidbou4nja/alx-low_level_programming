be8b8d3aa250bd49154036ee3dc6c5320c8621a7
raxidbou4nja <official.wetyn@gmail.com>
2023-07-20 17:57:57 -0700

    add new file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,46 +6,47 @@
 /**
  * print_all - prints all
  * @format: format string that says arg types
- */
+*/
 
 void print_all(const char * const format, ...)
 {
-	char *s, c;
-	double f;
-	int arg_count = 0, is_first_arg = 1, i;
 	va_list args;
+	float f;
+	char *s, c;
+	int arg_count = 0, i;
 
 	va_start(args, format);
 	while (format[arg_count] != '\0')
 	{
-		if (format[arg_count] == 'c')
+		switch (format[arg_count])
 		{
-			c = va_arg(args, int);
-			printf("%s%c", is_first_arg ? "" : ", ", c);
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char*);
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
+				break;
 		}
-		else if (format[arg_count] == 'i')
-		{
-			i = va_arg(args, int);
-			printf("%s%d", is_first_arg ? "" : ", ", i);
-		}
-		else if (format[arg_count] == 'f')
-		{
-			f = va_arg(args, double);
-			printf("%s%f", is_first_arg ? "" : ", ", f);
-		}
-		else if (format[arg_count] == 's')
-		{
-			s = va_arg(args, char*);
-			if (s == NULL)
-			{
-				printf("%s(nil)", is_first_arg ? "" : ", ");
-			} else
-			{
-				printf("%s%s", is_first_arg ? "" : ", ", s);
-			}
-		}
-		is_first_arg = 0;
 		arg_count++;
+		if (format[arg_count] != '\0')
+		{
+			if (format[arg_count] == 'c' || format[arg_count] == 'i' ||
+				format[arg_count] == 'f' || format[arg_count] == 's')
+				printf(", ");
+		}
 	}
 	va_end(args);
 	printf("\n");
