506f7986dca6e216839348d82a859c21214c71fb
raxidbou4nja <official.wetyn@gmail.com>
2023-07-20 18:48:54 -0700

    update files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,29 +5,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int print_c(va_list a);
-int print_i(va_list a);
-int print_f(va_list a);
-int print_s(va_list a);
-/**
- * struct op - struct for 3
- * @c: character to check
- * @f: function to point to
- */
-typedef struct op
+
+typedef struct
 {
-	char *c;
-	int (*f)(va_list a);
-} printer;
-
-typedef struct {
-    char type;
-    void (*printer)(va_list);
+	char type;
+	void (*printer)(va_list);
 } types_t;
-
-void print_all(const char * const format, ...);
-void print_int(va_list args);
-void print_char(va_list args);
-void print_string(va_list args);
-void print_float(va_list args);
 #endif
