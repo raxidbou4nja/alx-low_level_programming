c2780f7aa8c812f46db6197f32339f9774eba996
raxidbou4nja <official.wetyn@gmail.com>
2023-08-01 15:10:06 -0700

    add task 9 files

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t)), *current;
-        unsigned int i;
+	unsigned int i;
 
 	if (new_node == NULL)
 		return (NULL);
