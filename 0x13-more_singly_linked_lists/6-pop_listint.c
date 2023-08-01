dd8616231547c2a9c5b569e689979d5e41928cd0
raxidbou4nja <official.wetyn@gmail.com>
2023-08-01 14:21:30 -0700

    add task 6 file

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * pop_listint - Removes the first node of a linked list of integers.
+ *
+ * @head: Pointer to a pointer to the head of the linked list.
+ *
+ * Return: The integer value stored in the removed node.
+ *		 If the linked list is empty, returns 0.
+ */
+int pop_listint(listint_t **head)
+{
+	int value;
+
+	if (head == NULL || *head == NULL)
+	{
+		return 0;
+	}
+
+	value = (*head)->n;
+	listint_t *temp = *head;
+	*head = (*head)->next;
+	free(temp);
+
+	return (value);
+}
