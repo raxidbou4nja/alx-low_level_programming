c6f8c60667136c55c1af25eda8ee5a422501dd93
raxidbou4nja <official.wetyn@gmail.com>
2023-08-02 10:44:40 -0700

    add task 12 file

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints a linked list of integers, even with a loop.
+ *
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the list.
+ *
+ * Description:
+ * This function prints the elements of a linked
+ * list of integers, even if the list has a loop.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current = head;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		current = current->next;
+
+		/* Check for a loop */
+		if (count > 1024)
+		{
+			printf("-> [%p] (loop detected)\n", (void *)current);
+			exit(98);
+		}
+	}
+
+	return (count);
+}
