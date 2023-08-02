501576578f61f07e9961f468407920791f88c76a
raxidbou4nja <official.wetyn@gmail.com>
2023-08-02 11:20:28 -0700

    fix 12 task file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,22 +16,19 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
-	size_t count = 0;
-
-	while (current != NULL)
+	if (head)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-
-		if ((void *)current <= (void *)current->next)
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (head->next < head)
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			exit(98);
+			return (1 + print_listint_safe(head->next));
+		}
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			return (1);
 		}
 
-		current = current->next;
 	}
-
-	return (count);
+	return (0);
 }
