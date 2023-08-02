ad3e20299811c3a44be49bb6ae0566052d4ccf7f
raxidbou4nja <official.wetyn@gmail.com>
2023-08-02 10:16:04 -0700

    add task 11 file

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - Finds the beginning node of a loop in a linked list.
+ *
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: Pointer to the beginning node of the loop if a loop is found.
+ * If there is no loop, returns NULL.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
