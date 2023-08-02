08b01762fe29916f4071d96a3455c364090d7382
raxidbou4nja <official.wetyn@gmail.com>
2023-08-02 11:24:23 -0700

    reverse file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,19 +16,18 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (head->next < head)
-		{
-			return (1 + print_listint_safe(head->next));
-		}
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			return (1);
-		}
+	if (head == NULL)
+		return (0);
 
+	printf("[%p] %d\n", (void *)head, head->n);
+
+	if (head->next < head)
+	{
+		return (1 + print_listint_safe(head->next));
 	}
-	return (0);
+	else
+	{
+		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+		return (1);
+	}
 }
