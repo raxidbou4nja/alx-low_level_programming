95ed415e18adf1a2c4430fb0ff03e731fa15add4
raxidbou4nja <official.wetyn@gmail.com>
2023-08-03 16:43:39 -0700

    update task 1 file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,15 +4,15 @@
 /**
  * print_binary - Prints Binary of a number
  *
- * @num: A pointer to a number
+ * @n: A pointer to a number
  *
  */
 
-void print_binary(unsigned int num)
+void print_binary(unsigned long int n)
 {
-	if (num > 1)
+	if (n > 1)
 	{
-		print_binary(num >> 1);
+		print_binary(n >> 1);
 	}
-	_putchar('0' + (num & 1));
+	_putchar('0' + (n & 1));
 }
