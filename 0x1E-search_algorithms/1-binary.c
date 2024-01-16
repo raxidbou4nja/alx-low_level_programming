bc72903bb0c454c33289633d14ee8d015874a22b
raxidbou4nja <rachid.boughnja@gmail.com>
2024-01-16 08:19:32 -0800

    add new file

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - Searches for a value in a sorted array using binary search
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ * Return: Index of the value in the array, or -1 if not found
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0, right = size - 1, mid, i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+			{
+				printf("%d\n", array[i]);
+			}
+			else
+			{
+				printf("%d, ", array[i]);
+			}
+		}
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+
+	return (-1);
+}
